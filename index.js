b0123949117a6dd89085b02810d5ef2ca6381ee5
badillosoft <badillo.soft@hotmail.com>
2020-08-19 15:02:51 -0500

    add stripe

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const mongo = require("./mongo");
 const util = require("./util");
+const stripe = require("stripe");
 
 module.exports = {
     mongo,
     ...mongo,
     util,
-    ...util
+    ...util,
+    stripe
 };
