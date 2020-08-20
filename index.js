ce99045f154d3e62d11abb0208364887d30d0b41
badillosoft <badillo.soft@hotmail.com>
2020-08-20 11:04:15 -0500

    add uuid

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const mongo = require("./mongo");
 const util = require("./util");
 const stripe = require("stripe");
+const { v4: uuid } = require("uuid");
 
 module.exports = {
     mongo,
     ...mongo,
     util,
     ...util,
-    stripe
+    stripe,
+    uuid
 };
