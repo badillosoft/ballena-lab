7b2a30680bb3137c156954bf2691dcb2956a8deb
badillosoft <badillo.soft@hotmail.com>
2020-09-04 19:25:16 -0500

    add crypto binary

--- crypto/index.js
+++ crypto/index.js
@@ -12,5 +12,17 @@ module.exports = {
         let decrypted = decipher.update(text, "hex", "utf8");
         decrypted += decipher.final("utf8");
         return decrypted;
-    }
+    },
+    encryptBinary(text, password) {
+        const cipher = crypto.createCipher("aes-256-cbc", password);
+        let crypted = cipher.update(text, "binary", "hex");
+        crypted += cipher.final("hex");
+        return crypted;
+    },
+    decryptBinary(text, password) {
+        const decipher = crypto.createDecipher("aes-256-cbc", password);
+        let decrypted = decipher.update(text, "hex", "binary");
+        decrypted += decipher.final("binary");
+        return decrypted;
+    },
 };
